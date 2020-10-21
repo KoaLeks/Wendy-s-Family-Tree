ba434118a7e58628751bea0066281d252bc890ce
Aleksandar Marinkovic <e1634028@student.tuwien.ac.at>
2020-10-21 15:22:18 +0200

    Story 26: Added more test data to insertData.sql.

--- backend/src/main/resources/sql/createSchema.sql
+++ backend/src/main/resources/sql/createSchema.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS breed
 (
-  id                BIGINT AUTO_INCREMENT PRIMARY KEY,
+  id          BIGINT AUTO_INCREMENT PRIMARY KEY,
   name              VARCHAR(255) NOT NULL,
   description       VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS horse
 (
-  id            BIGINT AUTO_INCREMENT PRIMARY KEY,
-  name          VARCHAR(255)     NOT NULL,
-  description   VARCHAR(255),
-  birth_date    DATETIME         NOT NULL,
-  is_male       BOOLEAN          NOT NULL,
-  breed_id      BIGINT           REFERENCES breed(id),
-  breed_name    VARCHAR(255),
-  father_id     BIGINT,
-  mother_id     BIGINT
+  id                          BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name                        VARCHAR(255)          NOT NULL,
+  description                 VARCHAR(255),
+  birth_date                  DATETIME              NOT NULL,
+  is_male                     BOOLEAN               NOT  NULL,
+  breed_id                    BIGINT,
+  father_id                   BIGINT,
+  mother_id                   BIGINT,
+  FOREIGN KEY (breed_id)      REFERENCES breed(id)
 );
--- backend/src/main/resources/sql/insertData.sql
+++ backend/src/main/resources/sql/insertData.sql
@@ -1,13 +1,27 @@
 -- insert initial test data
 -- the id is hardcoded to enable references between further test data
 INSERT INTO breed (ID, NAME, DESCRIPTION)
-VALUES (1, 'Arabian', 'a'),
-       (2, 'Morgan', 'm'),
-       (3, 'Paint', 'p');
+VALUES (1, 'Arabian', 'Originated on the Arabian Peninsula'),
+       (2, 'Morgan', ''),
+       (3, 'Paint', ''),
+       (4, 'Danish Warmblood', 'Sport horse breed from Denmark'),
+       (5, 'Giara', 'Native to the island of Sardinia'),
+       (6, 'Banker', 'Feral horses living on barrier islands in North Carolina'),
+       (7, 'Lokai', 'Mountain horse in Tajikistan, used as riding/packhorse'),
+       (8, 'Danube Delta', 'Feral horses in Romania'),
+       (9, 'Noma', 'Critically-endangered Japanese breed'),
+       (10, 'Spanish Barb', '');
 
-INSERT INTO horse (ID, NAME, DESCRIPTION, BIRTH_DATE, IS_MALE, BREED_ID, BREED_NAME, FATHER_ID, MOTHER_ID)
-VALUES (1, 'Joesph', 'lazy, old', '2017-10-03', true, 2, 'Morgan', 0, 0),
-       (2, 'Maria', '', '2011-06-23', false, 1, 'Arabian'),
-       (3, 'Alberto', 'nervös, neu', '2011-06-23', true, null, null);
+INSERT INTO horse (ID, NAME, DESCRIPTION, BIRTH_DATE, IS_MALE, BREED_ID, FATHER_ID, MOTHER_ID)
+VALUES (1, 'Joesph', 'lazy, old',   '2017-10-03', true,  2, 10, null),
+       (2, 'Maria', 'fast',         '2011-06-23', false, 1, null, null),
+       (3, 'Alberto', 'nervös',     '2015-01-31', true,  3, null, 6),
+       (4, 'Linda', 'schlachter',   '2011-12-21', false, 4, null, 2),
+       (5, 'Charlie', '',           '2018-09-11', true,  2, 3, 4),
+       (6, 'Mimi', '',              '1931-03-17', false,  1, null, null),
+       (7, 'Jenny', '',             '2020-04-29', false, 1, null, 6),
+       (8, 'Jimi', '',              '2019-06-23', true,  5, 1, 2),
+       (9, 'Kiki', '',              '2019-06-23', false, 5, 1, 2),
+       (10, 'Ronald', '',           '2015-11-13', true,  4, 3, null);
 
 
